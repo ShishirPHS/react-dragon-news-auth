a1f7e60d4b08af10713836e11dd8d60602f5000e
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-10-04 01:31:49 +0600

    added moment js

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,14 +1,16 @@
 import logo from "../../../assets/logo.png";
+import moment from "moment";
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center mt-14 mb-8">
       <img src={logo} alt="Logo" />
       <h3 className="text-lg text-[#706F6F] mt-5">
         Journalism Without Fear or Favour
       </h3>
       <p className="mt-3 text-[#706F6F] text-xl font-medium">
-        Sunday, November 27, 2022
+        <span className="text-[#403F3F]">{moment().format("dddd,")} </span>
+        {moment().format("MMMM D, YYYY")}
       </p>
     </div>
   );
