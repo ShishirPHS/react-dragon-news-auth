1cae7c46f01e89c31dff116e842b6175a1ab1018
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-10-06 15:56:20 +0600

    designed newsDetails page

--- src/pages/NewsDetails/NewsDetails.jsx
+++ src/pages/NewsDetails/NewsDetails.jsx
@@ -1,7 +1,25 @@
+import Header from "../shared/Header/Header";
+import RightSideNav from "../shared/RightSideNav/RightSideNav";
+import Navbar from "../shared/Navbar/Navbar";
+
 const NewsDetails = () => {
   return (
-    <div>
-      <h2 className="text-2xl">News Details</h2>
+    <div className="container mx-auto">
+      <div>
+        <Header></Header>
+        <Navbar></Navbar>
+      </div>
+      <div className="grid grid-cols-4 gap-6">
+        <div className="col-span-3">
+          <h2 className="text-xl text-[#403F3F] font-semibold mb-5">
+            Dragon News
+          </h2>
+          <div className="border p-7 rounded-md"></div>
+        </div>
+        <div>
+          <RightSideNav></RightSideNav>
+        </div>
+      </div>
     </div>
   );
 };
